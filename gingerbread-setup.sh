cf1c0a202cfc59abc0d471e3978f9beeee53de14
brandon15811 <brandon15811@gmail.com>
2012-05-07 00:40:50 -0500

    Update gingerbread-setup.sh

--- gingerbread-setup.sh
+++ gingerbread-setup.sh
@@ -1,5 +1,2 @@
 vendor/cyanogen/get-rommanager
 #mkdir -p vendor/cyanogen/proprietary && touch vendor/cyanogen/proprietary/RomManager.apk
-sudo mount --bind /home/ubuntu/system/ $WORKSPACE/$REPO_BRANCH/device/motorola/olympus/system
-cd $WORKSPACE/$REPO_BRANCH/device/motorola
-./local-extract-files.sh
