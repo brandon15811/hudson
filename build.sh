c6139446e4be70d21affc55db885fdb2da7d023c
brandon15811 <brandon15811@gmail.com>
2012-05-06 23:45:48 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -83,7 +83,7 @@ HUDSON_DIR=$WORKSPACE/hudson
 cp $HUDSON_DIR/$REPO_BRANCH.xml $WORKSPACE/$REPO_BRANCH/.repo/local_manifest.xml
 
 echo Syncing...
-repo sync -j 16 -f 2>$WORKSPACE/archive/reposync.log
+repo sync -j 32 -f 2>$WORKSPACE/archive/reposync.log
 check_result repo sync failed.
 echo Sync complete.
 
