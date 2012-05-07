07e673df6a93b663b443667e69c0aa30034d69fc
brandon15811 <brandon15811@gmail.com>
2012-05-07 15:42:16 -0500

    Update ics-setup.sh

--- ics-setup.sh
+++ ics-setup.sh
@@ -1,1 +1,16 @@
+WORKSPACE=$1
+REPO_BRANCH=$2
+
 vendor/cm/get-prebuilts
+
+cd $WORKSPACE/$REPO_BRANCH
+
+#device patch
+cd vendor/cm
+git apply $WORKSPACE/$REPO_BRANCH/patch/vendor-cm
+
+#camera patch
+git apply $WORKSPACE/$REPO_BRANCH/patch/frameworks-base-camera
+
+#memory patch
+git apply $WORKSPACE/$REPO_BRANCH/patch/frameworks-base-memory
