f8e883a194c2a67d4a9597ac42e29dad5331c32d
brandon15811 <brandon15811@gmail.com>
2012-06-04 16:47:02 -0500

    Update defy

--- build.sh
+++ build.sh
@@ -141,8 +141,8 @@ fi
 rm -f $OUT/*.zip*
 make $CLEAN_TYPE
 
-mka -j$CORES bacon
-#make -j$CORES otapackage
+#mka -j$CORES bacon
+make -j$CORES otapackage
 check_result Build failed.
 
 echo "Files in $OUT"
